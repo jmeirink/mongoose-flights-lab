382f9aaa94dbabacd8f3b3928923ca4b65d838b6
Jesse Meirink <jesse@meirink.com>
2022-06-20 19:23:26 -0500

    modify modle schema

--- models/flight.js
+++ models/flight.js
@@ -3,11 +3,26 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
 const flightSchema = new Schema({
-  airline: String,
-  airport: String,
-  flightNo: Number,
-  departs: Date,
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United']
+  },
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+  },
+  flightNo: {
+    type: Number
+  },
+  departs: {
+    type: Date
+  }
 }, {
   timestamps: true,
 })
 
+const Flight = mongoose.model('Flight', flightSchema)
+
+export {
+  Flight
+}
